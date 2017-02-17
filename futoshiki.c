11ac7c32094e8a7c231cae5f7c2f0664be08cf67
dark1wador <dark1wador@ymail.com>
2017-02-17 12:00:35 +0100

    Fin lecture de grille

--- futoshiki.c
+++ futoshiki.c
@@ -2,26 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DEBUG 1
+#define DEBUG_INIT_CONST 0
+#define DEBUG_FILE_READING 1
+
+// Nombre attribuer aux domaines interdits
+#define NO_DOMAINE -1
 
 enum operateur {
-    DIF, SUP, INF
+	DIF, SUP, INF
 };
 
 typedef struct CASE {
-    // Index in grid
-    size_t ind;
-    int * domaine;
-    // indices de toutes les contraintes de la case dans le tableau contraintes
-    size_t * conts;
-    // indice du dernier indice ajouter au tableau precedant
-    size_t indLastConst;
+	// Index in grid
+	size_t ind;
+	int * domaine;
+	// indices de toutes les contraintes de la case dans le tableau contraintes
+	size_t * conts;
+	// indice du dernier indice ajouter au tableau precedant
+	size_t indLastConst;
 } CASE;
 
 typedef struct CONTRAINTE {
-    enum operateur op;
-    CASE * droite;
-    CASE * gauche;
+	enum operateur op;
+	CASE * droite;
+	CASE * gauche;
 } CONTRAINTE;
 
 size_t nbContraintes;
@@ -32,310 +36,391 @@ CASE * grid;
 size_t lineSize;
 
 FILE * openFile(char * fileName) {
-    FILE * grille = fopen(fileName, "r");
+	FILE * grille = fopen(fileName, "r");
 
-    return grille;
+	return grille;
 }
 
 void closeFile(FILE * file) {
-    fclose(file);
+	fclose(file);
 }
 
 char getOpChar(enum operateur op) {
-    switch (op) {
-        case DIF:
-            return '!';
-        case SUP:
-            return '<';
-        case INF:
-            return '>';
-        default:
-            return 'E';
-    }
+	switch (op) {
+		case DIF:
+			return '!';
+		case SUP:
+			return '>';
+		case INF:
+			return '<';
+		default:
+			return 'E';
+	}
 }
 
 void displayConstraint(size_t numConst, CONTRAINTE * constr) {
-    printf("CONSTRAINT num %zu\n", numConst);
-    char op = getOpChar(constr->op);
-    printf("    %zu", constr->gauche->ind);
-    printf(" %c ", op);
-    printf("%zu\n", constr->droite->ind);
+	printf("CONSTRAINT num %zu\n", numConst);
+	char op = getOpChar(constr->op);
+	printf("    %zu", constr->gauche->ind);
+	printf(" %c ", op);
+	printf("%zu\n", constr->droite->ind);
 }
 
 void displayCase(CASE *caseToDisplay) {
-    printf("CASE num %zu\n", caseToDisplay->ind);
-    printf("    domaine = {");
-    int i;
-    for (i = 0; i < lineSize; ++i) {
-        printf("%d", caseToDisplay->domaine[i]);
-        if (i < lineSize - 1)
-            printf(", ");
-    }
-    printf("}\n");
+	printf("CASE num %zu\n", caseToDisplay->ind);
+	printf("    domaine = {");
+	int i;
+	for (i = 0; i < lineSize; ++i) {
+		printf("%d", caseToDisplay->domaine[i]);
+		if (i < lineSize - 1)
+			printf(", ");
+	}
+	printf("}\n");
 }
 
 void displayGrid() {
-    size_t i;
-    for (i = 0; i < lineSize * lineSize; ++i) {
-        CASE * currentCase = &grid[i];
-        displayCase(currentCase);
-        printf("\n");
-    }
+	size_t i;
+	for (i = 0; i < lineSize * lineSize; ++i) {
+		CASE * currentCase = &grid[i];
+		displayCase(currentCase);
+		printf("\n");
+	}
 }
 
 void initAllCases(CASE * grid) {
-    size_t i;
-    for (i = 0; i < lineSize * lineSize; ++i) {
-        CASE currentCase;
+	size_t i;
+	for (i = 0; i < lineSize * lineSize; ++i) {
+		CASE currentCase;
 
-        currentCase.ind = i;
+		currentCase.ind = i;
 
-        // Initialisation du domaine
-        currentCase.domaine = malloc(lineSize * sizeof(int));
-        int m;
-        for (m = 0; m < lineSize; ++m) {
-            currentCase.domaine[m] = m + 1;
-        }
+		// Initialisation du domaine
+		currentCase.domaine = malloc(lineSize * sizeof (int));
+		int m;
+		for (m = 0; m < lineSize; ++m) {
+			currentCase.domaine[m] = m + 1;
+		}
 
-        currentCase.conts = malloc((2 * (lineSize - 1)) * sizeof(size_t));
-        currentCase.indLastConst = 0;
+		currentCase.conts = malloc((2 * (lineSize - 1)) * sizeof (size_t));
+		currentCase.indLastConst = 0;
 
-        grid[i] = currentCase;
-    }
+		grid[i] = currentCase;
+	}
 }
 
 void initBasicConstraints(CASE *grid) {
 
-    size_t numCurrentCase = 0;
-    size_t indEndLine;
-    size_t lastConstInd = 0;
-    int lineNum = 0;
+	size_t numCurrentCase = 0;
+	size_t indEndLine;
+	size_t lastConstInd = 0;
+	int lineNum = 0;
 
-    for (numCurrentCase = 0; numCurrentCase < lineSize * lineSize; ++numCurrentCase) {
-        CASE * currentCase = &grid[numCurrentCase];
+	for (numCurrentCase = 0; numCurrentCase < lineSize * lineSize; ++numCurrentCase) {
+		CASE * currentCase = &grid[numCurrentCase];
 
-        if (DEBUG) {
-            displayCase(currentCase);
-            printf("\n");
-        }
+		if (DEBUG_INIT_CONST) {
+			displayCase(currentCase);
+			printf("\n");
+		}
 
-        indEndLine = numCurrentCase;
+		indEndLine = numCurrentCase;
 
-        // Recherche de l'indice de la case de debut de la ligne si != debu de linge
-        if (indEndLine % lineSize == 0) {
-            indEndLine += lineSize;
-        } else {
-            while (!(indEndLine % lineSize == 0)) {
+		// Recherche de l'indice de la case de debut de la ligne si != debu de linge
+		if (indEndLine % lineSize == 0) {
+			indEndLine += lineSize;
+		}
+		else {
+			while (!(indEndLine % lineSize == 0)) {
 
-                indEndLine++;
-            }
-        }
+				indEndLine++;
+			}
+		}
 
-        if (DEBUG) {
-            printf("indEndLine : %zu\n", indEndLine);
-        }
+		if (DEBUG_INIT_CONST) {
+			printf("indEndLine : %zu\n", indEndLine);
+		}
 
-        // Ajout de toutes les contraintes de la ligne courante a 
-        // partir de la case courrante car les cases precedentes ont deja mis
-        // les contraintes sur celle-ci (pas de redondance des contraintes)
-        size_t indCaseDroite = numCurrentCase + 1;
-        for (; indCaseDroite < indEndLine; ++indCaseDroite) {
+		// Ajout de toutes les contraintes de la ligne courante a 
+		// partir de la case courrante car les cases precedentes ont deja mis
+		// les contraintes sur celle-ci (pas de redondance des contraintes)
+		size_t indCaseDroite = numCurrentCase + 1;
+		for (; indCaseDroite < indEndLine; ++indCaseDroite) {
 
-            CASE * caseDroite = &grid[indCaseDroite];
+			CASE * caseDroite = &grid[indCaseDroite];
 
-            CONTRAINTE cont;
-            cont.gauche = currentCase;
-            cont.droite = caseDroite;
-            cont.op = DIF;
+			CONTRAINTE cont;
+			cont.gauche = currentCase;
+			cont.droite = caseDroite;
+			cont.op = DIF;
 
-            contraintes[lastConstInd] = cont;
-            currentCase->conts[currentCase->indLastConst++] = lastConstInd;
-            caseDroite->conts[caseDroite->indLastConst++] = lastConstInd;
+			contraintes[lastConstInd] = cont;
+			currentCase->conts[currentCase->indLastConst++] = lastConstInd;
+			caseDroite->conts[caseDroite->indLastConst++] = lastConstInd;
 
-            if (DEBUG) {
-                printf("contrainte de ligne : ");
-                displayConstraint(lastConstInd, &cont);
-            }
-            lastConstInd++;
-        }
+			if (DEBUG_INIT_CONST) {
+				printf("contrainte de ligne : ");
+				displayConstraint(lastConstInd, &cont);
+			}
+			lastConstInd++;
+		}
 
-        if (numCurrentCase % lineSize == 0) {
-            lineNum++;
-        }
+		if (numCurrentCase % lineSize == 0) {
+			lineNum++;
+		}
 
-        // Ajout de toutes les contraintes de la colonne courante
-        int i = lineNum;
-        size_t lineCpt = 1;
-        for (; i < lineSize; ++i) {
+		// Ajout de toutes les contraintes de la colonne courante
+		int i = lineNum;
+		size_t lineCpt = 1;
+		for (; i < lineSize; ++i) {
 
-            if (DEBUG) {
-                printf("lineNum = %d, i = %d\n", lineNum, i);
-            }
+			if (DEBUG_INIT_CONST) {
+				printf("lineNum = %d, i = %d\n", lineNum, i);
+			}
 
-            CASE * caseDroite = &grid[numCurrentCase + (lineSize * lineCpt)];
+			CASE * caseDroite = &grid[numCurrentCase + (lineSize * lineCpt)];
 
-            CONTRAINTE cont;
-            cont.gauche = currentCase;
-            cont.droite = caseDroite;
-            cont.op = DIF;
+			CONTRAINTE cont;
+			cont.gauche = currentCase;
+			cont.droite = caseDroite;
+			cont.op = DIF;
 
-            contraintes[lastConstInd] = cont;
-            currentCase->conts[currentCase->indLastConst++] = lastConstInd;
-            caseDroite->conts[caseDroite->indLastConst++] = lastConstInd;
+			contraintes[lastConstInd] = cont;
+			currentCase->conts[currentCase->indLastConst++] = lastConstInd;
+			caseDroite->conts[caseDroite->indLastConst++] = lastConstInd;
 
-            if (DEBUG) {
-                printf("contrainte de colonne : ");
-                displayConstraint(lastConstInd, &cont);
-            }
-            lastConstInd++;
-            lineCpt++;
-        }
+			if (DEBUG_INIT_CONST) {
+				printf("contrainte de colonne : ");
+				displayConstraint(lastConstInd, &cont);
+			}
+			lastConstInd++;
+			lineCpt++;
+		}
 
-    }
+	}
 
-    if (DEBUG) {
-        printf("nbConstr : %zu, lastConst : %zu\n", nbContraintes, lastConstInd);
-    }
+	if (DEBUG_INIT_CONST) {
+		printf("nbConstr : %zu, lastConst : %zu\n", nbContraintes, lastConstInd);
+	}
 }
 
 int failInitGrid(char *line, char *msg) {
-    fprintf(stderr, "%s\n", msg);
-    if (line)
-            free(line);
-    return -1;
+	fprintf(stderr, "%s\n", msg);
+	if (line)
+		free(line);
+	return -1;
 }
 
-int initGrid(FILE *file) {
-    char * line = NULL;
-    size_t len = 0;
-    ssize_t read;
+int initGrid(FILE *gridFile) {
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-    read = getline(&line, &len, file);
-    if (read <= 0) {
-        return failInitGrid(line, "Grid file empty.");
-    }
+	read = getline(&line, &len, gridFile);
+	if (read <= 0) {
+		return failInitGrid(line, "Grid file empty.");
+	}
 
-    if (sscanf(line, "%zu", &lineSize) != 1) {
-        return failInitGrid(line, "Grid size must be a number.");
-    }
+	if (sscanf(line, "%zu", &lineSize) != 1) {
+		return failInitGrid(line, "Grid size must be an unsigned number.");
+	}
 
-    if (DEBUG) 
-        printf("Grid size : %zu\n", lineSize);
+	if (DEBUG_INIT_CONST)
+		printf("Grid size : %zu\n", lineSize);
 
-    grid = malloc((lineSize * lineSize) * sizeof (CASE));
+	grid = malloc((lineSize * lineSize) * sizeof (CASE));
 
-    nbContraintes = ((lineSize - 1) * lineSize * lineSize);
-    contraintes = malloc(nbContraintes * sizeof (CONTRAINTE));
+	nbContraintes = ((lineSize - 1) * lineSize * lineSize);
+	contraintes = malloc(nbContraintes * sizeof (CONTRAINTE));
 
-    // initialisation des cases
-    initAllCases(grid);
+	// initialisation des cases
+	initAllCases(grid);
 
-    if (DEBUG) {
-        printf("Grid initialized\n");
-        displayGrid();
-        printf("\n");
-    }
-    // initialisation des contraintes
-    initBasicConstraints(grid);
+	if (DEBUG_INIT_CONST) {
+		printf("Grid initialized\n");
+		displayGrid();
+		printf("\n");
+	}
+	// initialisation des contraintes
+	initBasicConstraints(grid);
 
-    /*int isLineWithNumber = 1;
-    numCurrentCase = 0;
-    while ((read = getline(&line, &len, file)) != -1) {
-            printf("Retrieved line of length %zu :\n", read);
-            printf("%s\n", line);
- 
-            //int isNumDecide = 0;
-            int j = 0;
-            for (; j < len; j++){
-                    if (line[j] != ' '){
-                            if (line[j] == '>' || line[j] == '<'){
-	    			
-                                    /*int k = 0;
-                                    for (; k < gridSize * gridSize; k++){
-                                            if (cases[k].x == j - 1){
-	    				
-                                            }
-                                                    //contraintes
-                                    }/*
-                            }
-                            else {
-                                    if (isLineWithNumber) {
-                                            /*CASE thisCase;
-                                            thisCase.x = compt;
-                                            thisCase.y = j / 2;
+	if (line)
+		free(line);
 
-                                            if (!isNumDecide){
-                                                    int dom[gridSize];
-                                                    int i;
-                                                    for (i = 0; i < gridSize; i++)
-                                                            dom[i] = i + 1;
-                                                    thisCase.domaine = dom;
-                                            }
-                                            else {
-                                                    int dom[1];
-                                                    dom = current;
-                                                    thisCase.domaine = dom;
-                                            }/*
-							    	
-                                            isLineWithNumber = 0;
-                                            numCurrentCase++;
-                                    }
-                                    else {
-				
-                                            isLineWithNumber = 1;
-                                    }
-                            }
-                    }
-            }
-        
-            compt++;
-    }*/
+	return 1;
+}
 
-    if (line)
-        free(line);
+int parseNumberLine(char* line, size_t lineNumber) {
+	size_t charNumber;
+	for (charNumber = 0; charNumber < lineSize * 2; ++charNumber) {
+		// Si on doit changer une contrainte
+		if (line[charNumber] == '<' || line[charNumber] == '>') {
+			if (charNumber == 0 || charNumber == lineSize * 2) {
+				return failInitGrid(line, "Constraint without both values detected");
+			}
+			CASE *left = &grid[charNumber / 2 + (lineNumber / 2) * lineSize];
+			
+			// Recherche de la premiere contrainte ou left est a gauche
+			// Sachant que ce sera au minium la premiere apres le nombre de case a gauche
+			size_t i;
+			CONTRAINTE *constraint;
+			for (i = charNumber / 2; ; ++i) {
+				constraint = &contraintes[left->conts[i]];
+				if (constraint->gauche == left) {
+					break;
+				}
+			}
+			
+			if(line[charNumber] == '<')
+				constraint->op = INF;
+			else 
+				constraint->op = SUP;
+			
+			if (DEBUG_FILE_READING) {
+				displayConstraint(left->conts[i], constraint);
+			}
+		}
+		// C'est un nombre
+		else if (line[charNumber] != ' ') {
+			int number;
+			if (sscanf(&line[charNumber], "%d", &number) != 1) {
+				return failInitGrid(line, "Unknown char detected on a number line");
+			}
+			
+			if (number != 0) {
+				CASE *currentCase = &grid[charNumber / 2 + (lineNumber / 2) * lineSize];
+				size_t i;
+				// On enleve toutes les valeurs du domaine
+				for (i = 0; i < lineSize; ++i) {
+					if (currentCase->domaine[i] == number)
+						continue;
+					currentCase->domaine[i] = NO_DOMAINE;
+				}
+				if (DEBUG_FILE_READING)
+					displayCase(currentCase);
+			}
+		}
+	}
+	return 1;
+}
 
-    return 1;
+int parseConstraintOnlyLine(char *line, size_t lineNumber) {
+	size_t charNumber;
+	for (charNumber = 0; charNumber < lineSize; ++charNumber) {
+		if (line[charNumber] != '.') {
+			CASE *left = &grid[charNumber + (lineNumber / 2) * lineSize];
+			CASE *right = &grid[charNumber + ((lineNumber + 1) / 2) * lineSize];
+			// Recherche la premiere contrainte de colonne ou left est a gauche et right est a droite
+			// On sait que ce sera au minimum la premiere apres le nombre de case a droite
+			CONTRAINTE *constraint;
+			size_t i;
+			for (i = (lineSize - 1) - charNumber; ; ++i) {
+				constraint = &contraintes[left->conts[i]];
+				if (constraint->gauche == left && constraint->droite == right) {
+					break;
+				}
+			}
+			if(line[charNumber] == '^') {
+				constraint->op = INF;
+			}
+			else if (line[charNumber] == 'v') {
+				constraint->op = SUP;
+			}
+			else {
+				return failInitGrid(line, "Unknown char detected on constraint only line");
+			}
+			
+			if (DEBUG_FILE_READING)
+				displayConstraint(left->conts[ (lineSize - 1) - charNumber], constraint);
+		}
+	}
+	return 1;
+}
+
+int readAndParseFile(FILE *gridFile) {
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	
+	int isLineWithNumber = 1;
+	size_t lineNumber;
+	for (lineNumber = 0; lineNumber < lineSize * 2 - 1; ++lineNumber) {
+		read = getline(&line, &len, gridFile);
+		if (read <= 0) {
+			return failInitGrid(line, "Grid file malformed");
+		}
+		
+		if (DEBUG_FILE_READING)
+			printf("line read :%s", line);
+		
+		if (len < lineSize * 2) {
+			return failInitGrid(line, "Line malformed");
+		}
+		
+		int res;
+		if (isLineWithNumber) {
+			res = parseNumberLine(line, lineNumber);
+			if (res < 0) {
+				return res;
+			}
+		}
+		else {
+			res = parseConstraintOnlyLine(line, lineNumber);
+			if (res < 0) {
+				return res;
+			}
+		}
+		
+		isLineWithNumber = 1 - isLineWithNumber;
+	}	
+	if (line)
+		free(line);
+	return 1;
 }
 
 void freeCase(CASE * caseToFree) {
 
-    free(caseToFree->domaine);
-    free(caseToFree->conts);
+	free(caseToFree->domaine);
+	free(caseToFree->conts);
 }
 
 void freeGridAndConstraints() {
-    if (DEBUG) 
-        printf("Liberation memoire\n");
-    
-    size_t i;
-    for (i = 0; i < lineSize * lineSize; ++i) {
-        freeCase(&grid[i]);
-    }
-    free(grid);
-    free(contraintes);
+	if (DEBUG_INIT_CONST)
+		printf("Liberation memoire\n");
+
+	size_t i;
+	for (i = 0; i < lineSize * lineSize; ++i) {
+		freeCase(&grid[i]);
+	}
+	free(grid);
+	free(contraintes);
 }
 
-// Penser a tout free parce que j'ai tout compris
-
 int main(int argc, char * argv[]) {
-    if (argc - 1 < 1) {
-        fprintf(stderr, "Error on argument. Usage :\nfutoshiki <GridfileName>\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc - 1 < 1) {
+		fprintf(stderr, "Error on argument. Usage :\nfutoshiki <GridfileName>\n");
+		exit(EXIT_FAILURE);
+	}
 
-    FILE *gridFile = openFile(argv[1]);
-    if (gridFile == NULL) {
-        fprintf(stderr, "Impossible d'ouvrir le fichier\n");
-        exit(EXIT_FAILURE);
-    }
+	FILE *gridFile = openFile(argv[1]);
+	if (gridFile == NULL) {
+		fprintf(stderr, "Impossible d'ouvrir le fichier\n");
+		exit(EXIT_FAILURE);
+	}
 
-    int res = initGrid(gridFile);
-    if (res < 0) {
-        fprintf(stderr, "Erreur de creation de la grille\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Liberation memoire
-    freeGridAndConstraints();
-    return (0);
+	int res = initGrid(gridFile);
+	if (res < 0) {
+		fprintf(stderr, "Erreur de creation de la grille\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	res = readAndParseFile(gridFile);
+	if (res < 0) {
+		fprintf(stderr, "Erreur de lecture du fichier\n");
+		goto end;
+	}
+
+	end:
+	// Liberation memoire
+	freeGridAndConstraints();
+	return (0);
 }
